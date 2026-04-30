08ebb997fa9e19e5834958d7e73de563f66169d4
Surya <73090443+vigneshsurya21796@users.noreply.github.com>
2026-04-30 14:34:24 +0530

    first commit

--- backend/src/routes/auth.routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { login, Register } = require("../contorllers/Authcontroller");
-const router = express.Router();
-router.route("/login").get(login);
-router.route("/Registe").get(Register);
-module.exports = router;
